1ff643ffd5cb5bda5caaba03d4b8aae6c09faa76
Nashmeyah Al-Rekabi <nashalrekabi@gmail.com>
2020-04-08 04:43:19 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,25 +1,19 @@
-class Patient
-  attr_accessor :appointments, :doctor, :patient
-
+class Patient 
+  attr_accessor :name, :doctor, :appointment
   @@all = []
-
   def initialize(name)
-    @name = name
+    @name = name 
     @@all << self
   end
-
-  def self.all
-    @@all
+  def self.all 
+    @@all 
+  end 
+  def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
   end
-
   def appointments
     Appointment.all.select {|appt| appt.patient == self}
   end
-
-  def new_appointment (date, doctor)
-    Appointment.new(doctor, self, date)
-  end
-
   def doctors
     appointments.map {|appt| appt.doctor}
   end
