9dda76147d1d8d59f72cacf0fe391e6478f55fbb
Nashmeyah Al-Rekabi <nashalrekabi@gmail.com>
2020-04-08 04:47:49 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,13 +1,13 @@
-class Patient
+class Patient 
   attr_accessor :name, :doctor, :appointment
   @@all = []
   def initialize(name)
-    @name = name
+    @name = name 
     @@all << self
   end
-  def self.all
-    @@all
-  end
+  def self.all 
+    @@all 
+  end 
   def new_appointment(doctor, date)
     Appointment.new(date, self, doctor)
   end
