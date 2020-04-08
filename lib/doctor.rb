692eb17bfbc2e62f079651df02183d42c437f61d
Nashmeyah Al-Rekabi <nashalrekabi@gmail.com>
2020-04-08 04:24:21 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -13,6 +13,10 @@ class Doctor
     @@all
   end
 
+  def patients
+    appointments.map {|appt| appt.patients}
+  end
+
   def new_appointment(date, name)
     Appointment.new(name, self, date)
   end
@@ -20,4 +24,5 @@ class Doctor
   def appointments
     Appointment.all.select {|appt| appt.doctor == self}
   end
+
 end
